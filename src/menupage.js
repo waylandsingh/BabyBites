a27c33b7b94fe2e4832f08fd35ad564687615fb0
waylandsingh <wmsingh@ucdavis.edu>
2022-07-19 18:19:31 -0700

    menu page improvements

--- src/menupage.js
+++ src/menupage.js
@@ -7,7 +7,46 @@ export default function menupage() {
     tabcontent.innerHTML='';
 
     const menu = document.createElement("div");
-    menu.innerHTML = '<p>Okay, we got really lazy about putting a menu together.. Sue us.</p>'
+    menu.innerHTML = `
+        <div id="menu">
+            <h1 class="jumbo">Blood 'n' Bites</h1>
+            <div id="appetizers" class="menu-section">
+                <h1 class="menu-title jumbo">smalls</h1>
+                <h1>
+                    <b>carpaccio</b> 
+                    <span class="menu-right menu-tag menu-dark-grey menu-round">$17.00</span>
+                    <p class="menu-description">Fresh ground Chuck with black garlic and emu egg, served with toast points and capers. Raw.</p>
+                </h1>
+            </div>
+            
+            <div id="mains" class="menu-section">
+                <h1 class="menu-title jumbo">bigs</h1>
+                <h1>
+                    <b>burger</b> 
+                    <span class="menu-right menu-tag menu-dark-grey menu-round">$26.00</span>
+                    <p class="menu-description">Choice of lean or rich ground, on a brioche bun with pickled beets, beefsteak tomato and crispy red lettuce. Served rare.</p>
+                </h1>
+
+            </div>
+
+            <div id="drinks" class="menu-section">
+                <h1 class="menu-title jumbo">liquids</h1>
+                <h1>
+                    <b>shaken</b> 
+                    <span class="menu-right menu-tag menu-dark-grey menu-round">$14.00</span>
+                    <p class="menu-description">Unfiltered, raw blood from a freshly tapped source; lots of body, complex flavors - satisfying.</p>
+                </h1>
+                <h1>
+                    <b>stirred</b> 
+                    <span class="menu-right menu-tag menu-dark-grey menu-round">$13.00</span>
+                    <p class="menu-description">Top-layer plasma from gently centrifuged donors. Clear in color, and full of essentials for healthy living.</p>
+                </h1>
+            </div>
+
+         
+            
+        </div>
+    `
     // put together more detailed menu page! practice HTML for the next block
     tabcontent.appendChild(menu);
 
