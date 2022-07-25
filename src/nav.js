8a46175e10b3ca4bb8edc1b15e0dcb94c8c7574a
waylandsingh <wmsingh@ucdavis.edu>
2022-07-25 10:48:30 -0700

    revised fonts, improved tab and image appearance

--- src/nav.js
+++ src/nav.js
@@ -2,7 +2,7 @@ export default function nav() {
     const container = document.getElementById("content")
     const nav = document.createElement("div");
 
-    nav.innerHTML = '<div class="topnav"><ul class="tab-list">    <li id="home"><a href="#">🩸Home</a></li>    <li id="menu"><a href="#">Menu</a></li>    <li id="contact"><a href="#">Contact</a></li>  </ul></div>'
+    nav.innerHTML = '<div class="topnav"><ul class="tab-list">    <li id="home"><a href="#"><h2>👶 Home</h2></a></li><li id="menu"><a href="#"><h2>🩸 Menu</h2></a></li><li id="contact"><a href="#"><h2>👄 Contact</h2></a></li>  </ul></div>'
     
     container.appendChild(nav)
 }
