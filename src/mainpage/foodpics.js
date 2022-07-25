ef232f8a490c83370dd529fdb7dedb3b00e0e47f
waylandsingh <wmsingh@ucdavis.edu>
2022-07-25 11:33:00 -0700

    hopefully actually imported image this time

--- src/mainpage/foodpics.js
+++ src/mainpage/foodpics.js
@@ -1,10 +1,11 @@
 // <div id="foodpics"><img src = "../src/vampire-food.jpg" /></div>
-import foodpics from "./foodpics";
+import vampchef from "./vampchef.jpg";
+
 export default function foodpics() {
     const content = document.getElementById("tabcontent");
-    const imgdiv = document.createElement("div");
-    imgdiv.id = 'foodpics'
-    imgdiv.innerHTML = '<img src = "../src/vampchef.jpg" />'
+    const imgdiv = document.createElement("img");
+    imgdiv.id = 'foodpics';
+    imgdiv.src = vampchef;
 
     return imgdiv;
 
